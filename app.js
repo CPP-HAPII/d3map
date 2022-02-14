ea7a4f8f3f3e8bc5e2912cf5fd4a8bd55612d8b0
dimakiling <dimakiling@cpp.edu>
2022-02-13 21:21:43 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,26 @@ const DUMMY_DATA = [
 const CHART_WIDTH = 600;
 const CHART_HEIGHT = 400;
 
+const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
+const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
+
 const chartContainer = d3
     .select('svg')
     .attr('width',CHART_WIDTH)
     .attr('height', CHART_HEIGHT);
 
-const chart =chartContainer.append('g');
+x.domain(DUMMY_DATA.map((d) => d.region));
+y.domain([0, d3.max(DUMMY_DATA, d => d.value) + 3]);
+
+const chart = chartContainer.append('g');
 
 chart
 .selectAll('.bar')
 .data(DUMMY_DATA)
 .enter()
 .append('rect')
-.classed('bar', true);
+.classed('bar', true)
+.attr('width', x.bandwidth())
+.attr('height', data => CHART_HEIGHT - y(data.value))
+.attr('x', data => x(data.region))
+.attr('y', data => y(data.value));
