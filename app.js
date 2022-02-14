eb83915d05d28dd3e36d15b40d977d887395a402
dimakiling <dimakiling@cpp.edu>
2022-02-13 23:27:45 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const DUMMY_DATA = [
-    { id: 'd1', region: 'USA', value: 10 },
+    { id: 'd1', region: 'Total Gross Water Use for Pomona 2020', value: 21,174 },
     { id: 'd2', region: 'India', value: 12 },
     { id: 'd3', region: 'China', value: 11 },
     { id: 'd4', region: 'Germany', value: 6 },
