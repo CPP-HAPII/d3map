c5221a61985993cad3b85d51eafc2321a2189f6c
dimakiling <dimakiling@cpp.edu>
2022-02-13 23:00:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const MARGINS = {top: 20, bottom: 10};
 const CHART_WIDTH = 600;
 const CHART_HEIGHT = 400 - MARGINS.top - MARGINS.bottom;
 
+let selectedData = DUMMY_DATA;
+
 const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
 const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
 
@@ -28,29 +30,42 @@ chart
 .attr('transform', `translate(0, ${CHART_HEIGHT})`)
 .attr('color', '#4f009e');
 
-chart
-.selectAll('.bar')
-.data(DUMMY_DATA)
-.enter()
-.append('rect')
-.classed('bar', true)
-.attr('width', x.bandwidth())
-.attr('height', data => CHART_HEIGHT - y(data.value))
-.attr('x', data => x(data.region))
-.attr('y', data => y(data.value));
+function renderChart() {
+    chart
+    .selectAll('.bar')
+    .data(selectedData, data => data.id)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', x.bandwidth())
+    .attr('height', data => CHART_HEIGHT - y(data.value))
+    .attr('x', data => x(data.region))
+    .attr('y', data => y(data.value));
 
-chart
-.selectAll('.label')
-.data(DUMMY_DATA)
-.enter()
-.append('text')
-.text((data) => data.value)
-.attr('x', data => x(data.region) + x.bandwidth() / 2)
-.attr('y', data => y(data.value) - 20)
-.attr('text-anchor', 'middle')
-.classed('label', true);
+    chart.selectAll('.bar').data(selectedData, data => data.id).exit().remove();
 
+    chart
+    .selectAll('.label')
+    .data(selectedData, data => data.id)
+    .enter()
+    .append('text')
+    .text((data) => data.value)
+    .attr('x', data => x(data.region) + x.bandwidth() / 2)
+    .attr('y', data => y(data.value) - 20)
+    .attr('text-anchor', 'middle')
+    .classed('label', true);
 
+    chart
+    .selectAll('.label')
+    .data(selectedData, (data) => data.id)
+    .exit()
+    .remove();
+}
+
+renderChart();
+
+let unselectedIds = [];
+ 
 const listItems = d3
 .select('#data')
 .select('ul')
@@ -61,4 +76,18 @@ const listItems = d3
 
 listItems.append('span').text(data => data.region)
 
-listItems.append('input').attr('type', 'checkbox').attr('checked', true);
+listItems
+.append('input')
+.attr('type', 'checkbox')
+.attr('checked', true)
+.on('change', (data) => {
+    if (unselectedIds.indexOf(data.id) === -1) {
+        unselectedIds.push(data.id);
+    } else {
+        unselectedIds = unselectedIds.filter(id => id !== data.id);
+    }
+    selectedData = DUMMY_DATA.filter(
+        (d) => unselectedIds.indexOf(d.id) === -1
+    );
+    renderChart();
+});
